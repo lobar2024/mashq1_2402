0dd84f842c9b6584794656c97613c91ede191711
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-02-24 16:32:00 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+class Click:
+    def pul_yechish(self, pul):
+        print(f'${pul}-Clickdan yechildi')
+
+
+class Xazna:
+    def pul_yechish(self, pul):
+        print(f'${pul}-Xaznadan yechildi')
+
+
+hizmatlar = [Click(), Xazna()]
+
+# hizmatlar[0].pul_yechish(100)
+# hizmatlar[1].pul_yechish(150)
